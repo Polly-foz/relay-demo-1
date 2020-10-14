7ac6626fe3dd7d5b56c91c2cc4225bfb4f20eb44
yangpeiya <yangpeiya.foz@gmail.com>
2020-10-14 18:02:47 +0800

    Issue: 完成提交comment功能

--- src/components/IssueCommentCommit.js
+++ src/components/IssueCommentCommit.js
@@ -1,5 +1,8 @@
 import React, {useRef} from 'react';
 import graphql from "babel-plugin-relay/macro";
+import {commitMutation} from 'react-relay';
+import RelayEnvironment from '../RelayEnvironment';
+
 const mutation = graphql`
     mutation IssueCommentCommitMutation($body: String!){
         addComment(input:{body:$body,subjectId:"MDU6SXNzdWU3MjEyNDYyNzA="}){
@@ -18,15 +21,39 @@ const mutation = graphql`
         }
     }
 `
-function IssueCommentCommit(){
+
+function addComment(environment, body) {
+    const variables = {
+        body,
+    };
+
+    commitMutation(
+        environment,
+        {
+            mutation,
+            variables,
+            onCompleted: (response, errors) => {
+                console.log('Response received from server.')
+            },
+            onError: err => console.error(err),
+        },
+    );
+}
+
+function IssueCommentCommit() {
     const inputEl = useRef(null)
-    const onButtonClick = ()=>{
-        console.log(inputEl.current.value)
+    const onButtonClick = () => {
+        const value = inputEl.current.value
+        if(!value || value.trim()===''){
+            alert("请输入要提交的comment")
+            return
+        }
+        addComment(RelayEnvironment,value)
     }
     return (
         <div>
             <input type="text" placeholder="enter a comment" ref={inputEl}/>
-            <button onClick={()=>onButtonClick()}>submit</button>
+            <button onClick={() => onButtonClick()}>submit</button>
         </div>
     )
 }
